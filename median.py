5bcd6765dc91adc84e10b45cc36ca01c195cc3bf
Onyi Dozie <onyiyechukwu.dozie@kcl.ac.uk>
2023-10-01 12:03:26 +0100

    without debug messages

--- median.py
+++ median.py
@@ -18,18 +18,13 @@ if length == 1:
     print (numbers[0])
 elif length%2 != 0:
     half = length / 2
-    print(f'printing out the first calculated half {half}')
     index = math.ceil(half)
-    print(f'rounded up half {median}')
-    print(f'the median of the list is {numbers[index-1]}')
+    print(numbers[index-1])
     #it is median-1 because list index starts at 0
 else:
     half = length /2
-    print(f'half {half}')
-    print (f' numbers[half-1] {numbers[half-1]}')
-    print (f' numbers[half] {numbers[half]}')
     median = (numbers[half-1]+numbers[half])/2
-    print (f'the median of this list is {median}')
+    print (median)
     
-#print(numbers)
+
 
